b3688d70a005410473baf0ec8d7fc8cd66d7b001
Abdullah AlAnsi <a.alansi@basgate.com>
2023-10-26 20:19:17 +0300

    Still working

--- search/all_19.js
+++ search/all_19.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['حساب_20التاجر_0',['دليل سياسات واجراءات حساب التاجر',['../d7/d12/_xD8_xAF_xD9_x84_xD9_x8A_xD9_x84-_xD8_xB3_xD9_x8A_xD8_xA7_xD8_xB3_xD8_xA7_xD8_xAA-_xD9_x88_xD8_x3ff8cbdeecf829960e779f61faf56486.html',1,'']]]
+  ['حساب_20التاجر_0',['دليل سياسات واجراءات حساب التاجر',['../d7/d12/_xD8_xAF_xD9_x84_xD9_x8A_xD9_x84-_xD8_xB3_xD9_x8A_xD8_xA7_xD8_xB3_xD8_xA7_xD8_xAA-_xD9_x88_xD8_x3ff8cbdeecf829960e779f61faf56486.html',1,'']]],
+  ['حساب_20التاجر_2011_1',['دليل سياسات واجراءات حساب التاجر 11',['../da/df7/_xD8_xAF_xD9_x84_xD9_x8A_xD9_x84-_xD8_xB3_xD9_x8A_xD8_xA7_xD8_xB3_xD8_xA7_xD8_xAA-_xD9_x88_xD8_xa5757c1e6111e954a274c6bfd4a085d0.html',1,'']]]
 ];
--- search/all_22.js
+++ search/all_22.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['واجراءات_20حساب_20التاجر_0',['دليل سياسات واجراءات حساب التاجر',['../d7/d12/_xD8_xAF_xD9_x84_xD9_x8A_xD9_x84-_xD8_xB3_xD9_x8A_xD8_xA7_xD8_xB3_xD8_xA7_xD8_xAA-_xD9_x88_xD8_x3ff8cbdeecf829960e779f61faf56486.html',1,'']]],
-  ['واحكام_20المنصة_1',['دليل اليات-شروط- واحكام المنصة',['../d3/df4/_xD8_xAF_xD9_x84_xD9_x8A_xD9_x84-_xD8_xA7_xD9_x84_xD9_x8A_xD8_xA7_xD8_xAA-_xD8_xB4_xD8_xB1_xD9_x49bc19f149b409744787fc5d35ba31fb.html',1,'']]]
+  ['واجراءات_20حساب_20التاجر_2011_1',['دليل سياسات واجراءات حساب التاجر 11',['../da/df7/_xD8_xAF_xD9_x84_xD9_x8A_xD9_x84-_xD8_xB3_xD9_x8A_xD8_xA7_xD8_xB3_xD8_xA7_xD8_xAA-_xD9_x88_xD8_xa5757c1e6111e954a274c6bfd4a085d0.html',1,'']]],
+  ['واحكام_20المنصة_2',['دليل اليات-شروط- واحكام المنصة',['../d3/df4/_xD8_xAF_xD9_x84_xD9_x8A_xD9_x84-_xD8_xA7_xD9_x84_xD9_x8A_xD8_xA7_xD8_xAA-_xD8_xB4_xD8_xB1_xD9_x49bc19f149b409744787fc5d35ba31fb.html',1,'']]]
 ];
